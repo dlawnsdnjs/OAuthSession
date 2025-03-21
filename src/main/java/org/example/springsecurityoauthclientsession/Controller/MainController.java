76ede79db03a3dfa1936a402c84c031337117e33
dlawnsdnjs <59200605+dlawnsdnjs@users.noreply.github.com>
2025-03-21 17:57:20 +0900

    setting

--- src/main/java/org/example/springsecurityoauthclientsession/Controller/MainController.java
+++ src/main/java/org/example/springsecurityoauthclientsession/Controller/MainController.java
@@ -1,2 +1,13 @@
-package org.example.springsecurityoauthclientsession.Controller;public class MainController {
+package org.example.springsecurityoauthclientsession.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainP(){
+        return "main";
+    }
 }
--- src/main/java/org/example/springsecurityoauthclientsession/Controller/MyController.java
+++ src/main/java/org/example/springsecurityoauthclientsession/Controller/MyController.java
@@ -1,2 +1,13 @@
-package org.example.springsecurityoauthclientsession.Controller;public class MyController {
+package org.example.springsecurityoauthclientsession.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/my")
+    public String myP(){
+        return "my";
+    }
 }
