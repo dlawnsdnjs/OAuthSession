09bae58647e2df6a34f047fa55ebc1c94d2e6cdb
dlawnsdnjs <59200605+dlawnsdnjs@users.noreply.github.com>
2025-03-24 16:35:10 +0900

    jdbc setting

--- src/main/java/org/example/springsecurityoauthclientsession/Configuration/SecurityConfig.java
+++ src/main/java/org/example/springsecurityoauthclientsession/Configuration/SecurityConfig.java
@@ -1,9 +1,11 @@
 package org.example.springsecurityoauthclientsession.Configuration;
 
 import org.example.springsecurityoauthclientsession.OAuth2.CustomClientRegistrationRepository;
+import org.example.springsecurityoauthclientsession.OAuth2.CustomOAuth2AuthorizedClientService;
 import org.example.springsecurityoauthclientsession.Service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,21 +16,27 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
-    private final CustomClientRegistrationRepository clientRegistrationRepository;
+    private final CustomClientRegistrationRepository customClientRegistrationRepository;
+    private final CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService;
+    private final JdbcTemplate jdbcTemplate;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomClientRegistrationRepository clientRegistrationRepository) {
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomClientRegistrationRepository clientRegistrationRepository, CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService, JdbcTemplate jdbcTemplate) {
         this.customOAuth2UserService = customOAuth2UserService;
-        this.clientRegistrationRepository = clientRegistrationRepository;
+        this.customClientRegistrationRepository = clientRegistrationRepository;
+        this.customOAuth2AuthorizedClientService = customOAuth2AuthorizedClientService;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, CustomClientRegistrationRepository customClientRegistrationRepository) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.
                 csrf(csrf->csrf.disable())
                 .formLogin(login->login.disable())
                 .httpBasic(basic->basic.disable())
                 .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
                         .clientRegistrationRepository(customClientRegistrationRepository.clientRegistrationRepository())
+                        .authorizedClientService(customOAuth2AuthorizedClientService.oAuth2AuthorizedClientService(jdbcTemplate, customClientRegistrationRepository.clientRegistrationRepository()))
                         .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService)))
                 .authorizeHttpRequests(auth -> auth
